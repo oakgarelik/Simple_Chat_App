2be67b793f5afc6b17fbff92e8157ba95e5837fe
Oak Garelik Grangé <awolftwofive@gmail.com>
2024-07-20 22:15:49 -0400

    First Commit, Starting point, added FilledButton

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(
+              onPressed: () {}, child: const Text('Click me')),
+        ),
+      ),
+    );
+  }
+}
