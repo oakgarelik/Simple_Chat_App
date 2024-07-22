bcf27b89f0c625dd16964ad5a125fbc695eda46d
Oak Garelik Grangé <awolftwofive@gmail.com>
2024-07-22 19:51:28 -0400

    added ListView for chat

--- lib/config/presentation/chat/chat_screen.dart
+++ lib/config/presentation/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -6,16 +7,50 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(4.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://cdn1.suno.ai/image_9abae681-ecc5-4774-ae3f-0e0de04ac773.png'),
-          ),
+      appBar: _SupBar(),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _SupBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: const Padding(
+        padding: EdgeInsets.all(4.0),
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(
+              'https://cdn1.suno.ai/image_9abae681-ecc5-4774-ae3f-0e0de04ac773.png'),
         ),
-        centerTitle: false,
-        title: const Text('Darling ♥️'),
+      ),
+      centerTitle: false,
+      title: const Text('Darling ♥️'),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            const Text('World')
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 3).theme(),
-      home: ChatScreen(),
+      home: const ChatScreen(),
     );
   }
 }
