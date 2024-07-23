03726285a72bc578fb65449669ba90c233749350
Oak Garelik Grangé <awolftwofive@gmail.com>
2024-07-22 21:52:59 -0400

    Configured her message bubble, i take a break to play overwatch (last mod. in her_message_bubble.dart, line 28

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 3).theme(),
+      theme: AppTheme(selectedColor: 16).theme(),
       home: const ChatScreen(),
     );
   }
