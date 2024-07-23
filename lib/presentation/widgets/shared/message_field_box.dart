684a6ec23dc720ae7abb1234d7dc5c0d836fff0b
Oak Garelik Grangé <awolftwofive@gmail.com>
2024-07-23 12:52:37 -0400

    final

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,21 +5,38 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
     final colors = Theme.of(context).colorScheme;
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: InputDecoration(
         enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: colors.primary, width: 3),
-            borderRadius: BorderRadius.circular(20)),
+            borderRadius: BorderRadius.circular(40)),
         filled: true,
         fillColor: colors.brightness == Brightness.light
             ? colors.primary.withOpacity(0.1)
             : colors.primary.withOpacity(0.5),
-        hintText: 'Type a message',
-        suffixIcon:
-            IconButton(onPressed: () {}, icon: const Icon(Icons.send_rounded)),
+        hintText: 'End your message with a "?"',
+        suffixIcon: IconButton(
+            onPressed: () {
+              final textValue = textController.value.text;
+              print('button: $textValue');
+              textController.clear();
+            },
+            icon: const Icon(Icons.send_rounded)),
       ),
+      onFieldSubmitted: (value) {
+        print('Submit value $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
     );
   }
 }
